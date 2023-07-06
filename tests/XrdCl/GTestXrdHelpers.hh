7b21722bd8e9d129e1a272f70886e2508500ab61
AngeloGalav <tekken342@gmail.com>
2023-07-06 13:18:39 +0000

    Add converted UtilsTest
    Adds converted UtilsTest from CPPUnit to GTest.
    Finally, it corrects a comment that was straight-up wrong in UtilsTest.cc.

--- tests/XrdCl/GTestXrdHelpers.hh
+++ tests/XrdCl/GTestXrdHelpers.hh
@@ -1,8 +1,19 @@
+/** Contains some of the macros found in CppUnitXrdHelpers.hh,
+ *  converted into GTest equivalent. 
+ * 
+ *  Created by Angelo Galavotti in 2023.
+ */
+
 #include <gtest/gtest.h>
 #include <XrdCl/XrdClXRootDResponses.hh>
 #include <cerrno>
 #include <cstring>
 
+/** @brief Equivalent of CPPUNIT_ASSERT_XRDST
+ * 
+ * Shows the code that we are asserting and its value 
+ * in the final evaluation. 
+ */
 #define GTEST_ASSERT_XRDST( x )                    \
 {                                                    \
   XrdCl::XRootDStatus _st = x;                       \
